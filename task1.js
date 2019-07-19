dfb1c70f8a77ed190bcbb489e58088fadf008f6c
dealenx <dealenx@gmail.com>
2019-07-20 02:16:11 +0700

    Первая задача решена

---
+++ task1.js
@@ -0,0 +1,29 @@
+function sequence(start = 0, step = 1) {
+    
+    let generatedFunctionSequence = generateSequence(start, step);
+
+    function* generateSequence(startSeq, stepSeq) {
+        let sum = startSeq;
+        yield startSeq;
+        while (true) {
+            sum += stepSeq;
+            yield sum;
+        }
+    }
+
+    return function() {
+        return generatedFunctionSequence.next().value;
+    };
+}
+
+var generator = sequence(10, 3);
+var generator2 = sequence(7, 1);
+
+console.log(generator()); // 10
+console.log(generator()); // 13
+
+console.log(generator2()); // 7
+
+console.log(generator()); // 16
+
+console.log(generator2()); // 8
