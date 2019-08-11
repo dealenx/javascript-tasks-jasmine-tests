627f3154da6e6e4aa5fa012ecbfcc1ff5d0868ef
dealenx <dealenx@gmail.com>
2019-08-11 23:27:27 +0700

    task 10

---
+++ task10.js
@@ -0,0 +1,13 @@
+let count = function(obj) {
+  return Object.keys(obj).length;
+};
+
+let a = { a: 1, b: 2 };
+console.log(count(a)); // 2
+let b = function() {};
+console.log(count(b)); // 0
+let c = [1, 2, 3];
+console.log(count(c)); // 3
+let d = [];
+d[100] = 1;
+console.log(count(d)); // 1
